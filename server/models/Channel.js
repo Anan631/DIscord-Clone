703b39e06d38b3aee5005b6d568250dfe08b2ab2
Anan631 <anan.kayed@gmail.com>
2026-05-24 00:41:59 +0300

    add channel model

---
+++ server/models/Channel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const channelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Channel', channelSchema);
