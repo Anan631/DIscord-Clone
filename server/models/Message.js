3a5fc0c3ef98f1d378eb763eac89ae090f967c60
Anan631 <anan.kayed@gmail.com>
2026-05-24 00:48:21 +0300

    add message model

---
+++ server/models/Message.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema(
+  {
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Channel',
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 2000,
+    },
+  },
+  { timestamps: true }
+);
+
+messageSchema.index({ channel: 1, createdAt: -1 });
+
+export default mongoose.model('Message', messageSchema);
